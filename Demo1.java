e225a8d60b24f3b968f14b9b77236ff6a4691302
bijitaghimire <84325748+bijitaghimire@users.noreply.github.com>
2021-05-29 18:12:25 +0545

    Add files via upload

---
+++ Demo1.java
@@ -0,0 +1,17 @@
+class Demo1{
+   public static void main(String[] args) {
+       System.out.println("Hello1");
+   }
+}
+
+class Demo2{
+   public static void main() {
+       System.out.println("Hello2");
+   }
+}
+
+class Demo3{
+   public static void main() {
+       System.out.println("Hello3");
+   }
+}
